8cd5bb45cf129f9f794b3544040e90eef0305cba
himanshu2285 <himanshu2k05m@gmail.com>
2025-08-30 11:27:45 +0530

    Updated some staticfiles settings

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -119,21 +119,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STORAGES = {
-    "staticfiles":{
-        "BACKEND":"whitenoise.storage.CompressedStaticFilesStorage",
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
-
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, "static"),
 ]
 
 # Default primary key field type
--- build_files.sh
+++ build_files.sh
@@ -3,12 +3,12 @@ echo "Build Start"
 #!/bin/bash
 
 # Install Python dependencies from requirements.txt
-python3 -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 # Run Django database migrations (if applicable)
 # python3.x manage.py migrate
 
 # Collect static files for deployment
-python3 manage.py collectstatic --no-input --clear
+python manage.py collectstatic --no-input --clear
 
 echo "Build Complete"
