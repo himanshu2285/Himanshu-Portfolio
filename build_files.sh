c9736b26c5a63a7a552ff50ddd81cb0aab9a3df1
himanshu2285 <himanshu2k05m@gmail.com>
2025-08-31 14:35:14 +0530

    python command not found fix

--- build_files.sh
+++ build_files.sh
@@ -3,13 +3,13 @@
 echo "Build Start"
 
 # Install Python dependencies from requirements.txt
-python -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 # Run Django database migrations (if applicable)
 # python3 manage.py migrate
 
 # Collect static files for deployment
-python manage.py collectstatic --no-input --clear
+python3 manage.py collectstatic --no-input --clear
 
 # Copy media files to staticfiles if they exist
 # if [ -d "media" ]; then
